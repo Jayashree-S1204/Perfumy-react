287b6a8a86f8204b65f8e41219d7ee376cf5ef30
Jayashree-S1204 <jayashreeshanmugam12@gmail.com>
2026-07-06 17:33:08 +0530

    updated project

---
+++ perfumyapp/src/components/Products.js
@@ -0,0 +1,24 @@
+import one from "../assets/images/one.jpg";
+import two from "../assets/images/two.jpg";
+import three from "../assets/images/three.jpg";
+function Products(){
+  return(
+     <div className="products">
+      <div className="box">
+         <img src={one}></img>
+        <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>    
+      </div>
+      <div className="box">
+         <img src={two}></img>
+        <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>    
+      </div>
+      <div className="box">
+         <img src={three}></img>
+        <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>    
+      </div>
+
+     </div>
+     
+  );
+}
+export default Products
